f1bcebb4ccabd0ade8e4a29dc78f2886a381a73f
absh606 <abhi606@gmail.com>
2021-07-17 16:02:00 -0400

    Intial commit.

---
+++ src/test/java/com/tsys/billings/discoveryserver/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsys.billings.discoveryserver.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
